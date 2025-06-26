9a6b26a239ec792f05b02e76be942a9afd7b1949
faishalam <faishalabdulmajid@gmail.com>
2025-06-26 14:33:34 +0700

    done

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ limit: "20mb", extended: true }));
 
 app.use(router);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 app.listen(port, () => {
   console.log(`[LOG] App listening on port ${port}`);
 });
