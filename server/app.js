1ae97138ba5de0461203fee473889f2f185d656e
faishalam <faishalabdulmajid@gmail.com>
2025-06-26 14:12:08 +0700

    fixing listen

--- server/app.js
+++ server/app.js
@@ -2,15 +2,18 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 const express = require("express");
-const router = require("./routers");
-const app = express();
 const cors = require("cors");
+const router = require("./routers");
+
+const app = express();
 
 app.use(cors());
-
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
 
 app.use(router);
 
-module.exports = app;
+const port = process.env.PORT || 3000;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`[LOG] App listening on port ${port}`);
+});
